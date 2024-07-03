3290ae43cf487d3ab8457c7abbc21e9e5ee67992
Chibuikem Akpaka <lextutor.dev@gmail.com>
2024-07-03 11:31:37 +0100

    Add public key to configuration.

--- Payment_Inegrations/Integrations/Model/Common/RemitaConfiguration.cs
+++ Payment_Inegrations/Integrations/Model/Common/RemitaConfiguration.cs
@@ -5,6 +5,7 @@
         public const string ConfigKey = "Remita";
         public string ApiKey { get; set; }
         public string MerchantId { get; set; }
+        public string PublicKey { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Url { get; set; }
